ebc4714475538ac7b0aaaa41d1bb0e30f885cd3e
preetibp202 <preetipatil559@gmail.com>
2026-08-18 20:47:16 +0530

    Add root URL redirect

--- Employee/urls.py
+++ Employee/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 
 urlpatterns = [
+      path('', lambda request: redirect('/home/')),
     path('admin/', admin.site.urls),
     path('home/', include('emp.urls')),
 ]
