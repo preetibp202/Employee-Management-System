145904858d563c950f4ea7accef9ed26f14e9680
preetibp202 <preetipatil559@gmail.com>
2026-08-20 00:15:34 +0530

    Fix static CSS and feedback page

--- Employee/settings.py
+++ Employee/settings.py
@@ -116,8 +116,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+# Media files (uploaded images)
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
